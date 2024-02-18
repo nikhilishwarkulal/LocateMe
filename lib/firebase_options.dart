1a20effb0d9fa2da1e8b9e3b8b6dc18c405c785d
Nikhil Ishwara <nikhilishwar2@gmail.com>
2024-02-18 13:39:24 +0400

    dsd

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,13 @@
-import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
+import "package:firebase_core/firebase_core.dart" show FirebaseOptions;
+import "package:flutter/foundation.dart"
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
+        "DefaultFirebaseOptions have not been configured for web - "
+        "you can reconfigure this by running the FlutterFire CLI again.",
       );
     }
     switch (defaultTargetPlatform) {
@@ -17,40 +17,40 @@ class DefaultFirebaseOptions {
         return ios;
       case TargetPlatform.macOS:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          "DefaultFirebaseOptions have not been configured for macos - "
+          "you can reconfigure this by running the FlutterFire CLI again.",
         );
       case TargetPlatform.windows:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          "DefaultFirebaseOptions have not been configured for windows - "
+          "you can reconfigure this by running the FlutterFire CLI again.",
         );
       case TargetPlatform.linux:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          "DefaultFirebaseOptions have not been configured for linux - "
+          "you can reconfigure this by running the FlutterFire CLI again.",
         );
       default:
         throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
+          "DefaultFirebaseOptions are not supported for this platform.",
         );
     }
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyD4ve1lvaKZZE2wUV81k7xD3va8a7VB4GY',
-    appId: '1:220815842354:android:acdb235ee22dde8ecbb02b',
-    messagingSenderId: '220815842354',
-    projectId: 'locate-me-747ad',
-    storageBucket: 'locate-me-747ad.appspot.com',
+    apiKey: "AIzaSyD4ve1lvaKZZE2wUV81k7xD3va8a7VB4GY",
+    appId: "1:220815842354:android:acdb235ee22dde8ecbb02b",
+    messagingSenderId: "220815842354",
+    projectId: "locate-me-747ad",
+    storageBucket: "locate-me-747ad.appspot.com",
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCAnLI4vmnvKoBDDA7KJvUng9-w0GUmQnI',
-    appId: '1:220815842354:ios:7fb3129585c5023bcbb02b',
-    messagingSenderId: '220815842354',
-    projectId: 'locate-me-747ad',
-    storageBucket: 'locate-me-747ad.appspot.com',
-    iosBundleId: 'com.locateme.app.locateme',
+    apiKey: "AIzaSyCAnLI4vmnvKoBDDA7KJvUng9-w0GUmQnI",
+    appId: "1:220815842354:ios:7fb3129585c5023bcbb02b",
+    messagingSenderId: "220815842354",
+    projectId: "locate-me-747ad",
+    storageBucket: "locate-me-747ad.appspot.com",
+    iosBundleId: "com.locateme.app.locateme",
   );
 }
